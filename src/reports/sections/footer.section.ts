e81bf689316c925c653c3df7e890b521dbc23919
joellarramendia <joellarramendia@gmail.com>
2026-09-09 17:28:23 -0300

    feat: add footer section for PDF generation with page numbering

---
+++ src/reports/sections/footer.section.ts
@@ -0,0 +1,11 @@
+import { Content, ContextPageSize } from "pdfmake/interfaces";
+
+export const footerSection = (currentPage: number, pageCount: number, pageSize: ContextPageSize): Content => {
+    return {
+        text: `Página ${currentPage} de ${pageCount}`,
+        alignment: 'right',
+        fontSize: 12,
+        bold: true,
+        margin: [0, 20, 20, 0]
+    }
+}
