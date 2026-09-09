036750639d71e834cdf5375444283fed1dbdbf1e
joellarramendia <joellarramendia@gmail.com>
2026-09-09 17:28:54 -0300

    refactor: improve header section structure and formatting

--- src/reports/sections/header.section.ts
+++ src/reports/sections/header.section.ts
@@ -1,40 +1,64 @@
-import { Content } from "pdfmake/interfaces";
+import { Column, Content } from "pdfmake/interfaces";
 import { DateFormatter } from "src/helpers";
 
 const logo: Content = {
-    image: 'src/assets/tucan-code-logo.png',
-    width: 100,
-    height: 100,
-    alignment: 'center',
-    margin: [0, 0, 0, 20]
+  image: 'src/assets/tucan-code-logo.png',
+  width: 100,
+  height: 100,
+  alignment: 'center',
+  margin: [0, 0, 0, 20]
+}
+
+const currentDate: Column = {
+  text: DateFormatter.getDDMMMMYYY(new Date()),
+  alignment: 'right',
+  margin: [20, 30],
+  width: 180
 }
 
 interface HeaderOptions {
-    title?: string
-    subTitle?: string
-    showLogo?: boolean
-    showDate?: boolean
+  title?: string
+  subTitle?: string
+  showLogo?: boolean
+  showDate?: boolean
 }
 
 export const headerSection = (options: HeaderOptions): Content => {
   const { title, subTitle, showLogo = true, showDate = true } = options;
 
   const headerLogo: Content = showLogo ? logo : { text: '' };
-  const headerDate: Content = showDate
-    ? {
-        text: DateFormatter.getDDMMMMYYY(new Date()),
-        alignment: 'right',
-        margin: [20, 20],
-      }
-    : { text: '' };
+  const headerDate: Content = showDate ? currentDate : { text: '' }
+
+  const headerSubtitle: Content = subTitle ? {
+    text: subTitle,
+    alignment: 'center',
+    margin: [0, 2, 0, 0],
+    style: {
+      bold: true,
+      fontSize: 16
+    }
+  } : { text: '' }
+
 
   const headerTitle: Content = title
     ? {
-        text: title,
-        style: {
-          bold: true,
+      stack: [
+        {
+          text: title,
+          alignment: 'center',
+          margin: [0, 15, 0, 0],
+          style: {
+            bold: true,
+            fontSize: 22
+          }
         },
-      }
+        headerSubtitle
+      ]
+      // text: title,
+      // style: {
+      //   bold: true,
+      // },
+    }
     : { text: '' };
 
   return {
