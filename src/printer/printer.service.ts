ea28cdf36d0510b615981979cc2c313f1cf266ac
joellarramendia <joellarramendia@gmail.com>
2026-09-08 21:55:17 -0300

    Add PrinterModule and PrinterService for PDF generation functionality

---
+++ src/printer/printer.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import PdfPrinter from 'pdfmake'
+import { BufferOptions, TDocumentDefinitions } from 'pdfmake/interfaces';
+
+
+const fonts = {
+    Roboto: {
+        normal: 'fonts/Roboto-Regular.ttf',
+        bold: 'fonts/Roboto-Medium.ttf',
+        italics: 'fonts/Roboto-Italic.ttf',
+        bolditalics: 'fonts/Roboto-MediumItalic.ttf'
+    }
+}
+
+@Injectable()
+export class PrinterService {
+    private printer = new PdfPrinter(fonts)
+
+    createPdf(
+        docDefenition: TDocumentDefinitions,
+        options: BufferOptions = {}
+    ): PDFKit.PDFDocument {
+        return this.printer.createPdfKitDocument(docDefenition, options)
+    }
+}
