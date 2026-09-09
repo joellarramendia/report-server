1fb59ef0290f7958e2f39172ae7991af3158270e
joellarramendia <joellarramendia@gmail.com>
2026-09-08 23:37:33 -0300

    Add employment letter generation and handle employee retrieval by ID

--- src/basic-report/basic-report.controller.ts
+++ src/basic-report/basic-report.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
 import type { Response } from 'express';
 
 import { BasicReportService } from './basic-report.service';
@@ -13,6 +13,29 @@ export class BasicReportController {
     const pdfDoc = this.basicReportService.hello()
     response.setHeader('Content-Type', 'application/pdf')
     pdfDoc.info.Title = 'Hola-Mundo.pdf'
+    pdfDoc.pipe(response)
+    pdfDoc.end()
+  }
+
+
+  @Get('employment-letter')
+  async employmentLetter(@Res() response: Response) {
+    const pdfDoc = this.basicReportService.employmentLetter()
+    
+    response.setHeader('Content-Type', 'application/pdf')
+    pdfDoc.info.Title = 'Employment-Letter.pdf'
+    pdfDoc.pipe(response)
+    pdfDoc.end()
+  }
+
+
+
+  @Get('employment-letter/:employeeId')
+  async employmentLetterById(@Res() response: Response, @Param('employeeId') employeeId: string) {
+    const pdfDoc = await this.basicReportService.employmentLetterById(+employeeId)
+    
+    response.setHeader('Content-Type', 'application/pdf')
+    pdfDoc.info.Title = 'Employment-Letter.pdf'
     pdfDoc.pipe(response)
     pdfDoc.end()
   }
--- src/basic-report/basic-report.service.ts
+++ src/basic-report/basic-report.service.ts
@@ -1,20 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrinterService } from 'src/printer/printer.service';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { getHelloWorldReport } from 'src/reports';
+import { getEmploymentLetter, getEmploymentLetterByIdReport, getHelloWorldReport } from 'src/reports';
 
 
 
 @Injectable()
 export class BasicReportService {
     constructor(
-        private readonly prisma: PrismaService,
-        private readonly printerService: PrinterService
-    ) {}
+        private readonly printerService: PrinterService,
+        private readonly prisma: PrismaService
+    ) { }
 
     hello() {
 
         const docDefenition = getHelloWorldReport()
+
+        const doc = this.printerService.createPdf(docDefenition)
+
+        return doc
+    }
+
+
+    employmentLetter() {
+        const docDefenition = getEmploymentLetter()
+
+        const doc = this.printerService.createPdf(docDefenition)
+
+        return doc
+    }
+
+
+    async employmentLetterById(employeeId: number) {
+        const employee = await this.prisma.employees.findUnique({
+            where: {
+                id: employeeId
+            }
+        })
+
+        if (!employee) throw new NotFoundException(`Employee with id ${employeeId} not found`)
+
+        const docDefenition = getEmploymentLetterByIdReport({
+            employerName: 'Fulano Mengano',
+            employerPosition: 'Software Enginner',
+            employeeName: employee.name,
+            employeePosition: employee.position,
+            employeeStartDate: employee.start_date,
+            employeeHours: employee.hours_per_day,
+            employeeWorkSchedule: employee.work_schedule,
+            employeeCompany: 'Tucan Code Corp'
+        })
 
         const doc = this.printerService.createPdf(docDefenition)
 
