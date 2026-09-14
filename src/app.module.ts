65e898951782415d5d49648710242dac4959c71c
joellarramendia <joellarramendia@gmail.com>
2026-09-14 15:58:47 -0300

    add ExtraReportsModule to the application module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { BasicReportModule } from './basic-report/basic-report.module';
 import { PrinterModule } from './printer/printer.module';
 import { StoreReportsModule } from './store-reports/store-reports.module';
+import { ExtraReportsModule } from './extra-reports/extra-reports.module';
 
 
 @Module({
@@ -16,7 +17,9 @@ import { StoreReportsModule } from './store-reports/store-reports.module';
     
     PrinterModule,
     
-    StoreReportsModule
+    StoreReportsModule,
+    
+    ExtraReportsModule
   ],
 })
 export class AppModule {}
