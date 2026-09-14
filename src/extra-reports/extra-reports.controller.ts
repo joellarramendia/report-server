0832552574fc8b1dd78da5a58a443fc099ee03e6
joellarramendia <joellarramendia@gmail.com>
2026-09-14 16:47:20 -0300

    add ExtraReports module, controller, and service for generating HTML and custom reports

---
+++ src/extra-reports/extra-reports.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Res } from '@nestjs/common';
+import { ExtraReportsService } from './extra-reports.service';
+import type { Response } from 'express';
+
+@Controller('extra-reports')
+export class ExtraReportsController {
+  constructor(private readonly extraReportsService: ExtraReportsService) { }
+
+  @Get('html-report')
+  async getHtmlReport(@Res() response: Response) {
+    const pdfDoc = this.extraReportsService.getHtmlReport()
+    response.setHeader('Content-Type', 'application/pdf')
+    pdfDoc.info.Title = 'Html-report.pdf'
+    pdfDoc.pipe(response)
+    pdfDoc.end()
+  }
+
+
+  @Get('my-report')
+  async getMyReport(@Res() response: Response) {
+    const pdfDoc = this.extraReportsService.getMy()
+    response.setHeader('Content-Type', 'application/pdf')
+    pdfDoc.info.Title = 'Billing-Report.pdf'
+    pdfDoc.pipe(response)
+    pdfDoc.end()
+  }
+
+}
---
+++ src/extra-reports/extra-reports.service.ts
@@ -0,0 +1,49 @@
+import fs from 'fs'
+
+import { Injectable } from '@nestjs/common';
+import { PrinterService } from 'src/printer/printer.service';
+import { getHtmlContent } from 'src/helpers/html-to-pdfmake';
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
+import { headerSection } from 'src/reports/sections/header.section';
+import { footerSection } from 'src/reports/sections/footer.section';
+import { getMyReport } from 'src/reports';
+
+
+@Injectable()
+export class ExtraReportsService {
+    constructor(private readonly printerService: PrinterService,
+    ) { }
+
+    getHtmlReport() {
+
+        const html = fs.readFileSync('src/reports/html/basic-03.html', 'utf8')
+
+        const content = getHtmlContent(html, {
+            client: 'Joel Larramendia'
+        })
+
+        const docDefenition: TDocumentDefinitions = {
+            pageMargins: [40, 110, 40, 60],
+            header: headerSection({
+                title: 'HTML to PDFMake',
+                subTitle: 'Convertir HTML a PDFMake'
+            }),
+            footer: footerSection,
+            content: content
+        }
+
+        const doc = this.printerService.createPdf(docDefenition)
+
+        return doc
+    }
+
+
+    getMy() {
+
+        const docDefenition = getMyReport()
+
+        const doc = this.printerService.createPdf(docDefenition)
+
+        return doc
+    }
+}
